84e12c0c861d213e1a62e15c2c65802983cf1eee
Avanthika <avanthikaa1207@gmail.com>
2024-08-02 12:58:16 +0530

    Resolved bugs

--- hello-world-project/src/app/app.component.spec.ts
+++ hello-world-project/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hello-world-project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello World');
   });
 });
